5ecde0a589a338a7dc6ab1c1dc2c6f316c5b07c0
Xaniell <ezetobechukwu06@gmail.com>
2024-09-28 16:48:19 +0100

    calculator

--- js/app.js
+++ js/app.js
@@ -8,19 +8,17 @@ figure.forEach((node) => {
     const valueofnode = node.innerText.trim();
     const theresult = result.innerText.trim();
     
-    
-    if (result.innerText.includes("|")) {
+    if (result.innerText.includes("")) {
       result.innerText = result.innerText.replace("|", "");
     }
 
-    
     if (valueofnode === "AC") {
-      result.innerText = " |"; 
+      result.innerText = "";
       lastInputType = ''; 
+      result.classList.add('no-blink'); 
       return;
     }
 
-    
     if (valueofnode == "=") {
       try {
         let solution = eval(theresult);
@@ -29,31 +27,32 @@ figure.forEach((node) => {
       } catch (error) {
         result.innerText = "Error |";
       }
+      result.classList.add('no-blink'); 
       return;
     }
 
-    
     const operators = ['+', '-', '*', '/'];
     if (operators.includes(valueofnode)) {
-      
       if (lastInputType !== 'number') {
         return; 
       }
       lastInputType = 'operator'; 
     } else {
-      
       lastInputType = 'number';
     }
 
-    
     if (theresult == "0" || theresult == "Infinity" || theresult == "undefined") {
       result.innerText = "";
     }
 
-    
     result.append(valueofnode);
 
-    
+    if (result.innerText.trim() !== '') {
+      result.classList.remove('no-blink');
+    } else {
+      result.classList.add('no-blink');
+    }
+
     node.blur();
   });
 });
